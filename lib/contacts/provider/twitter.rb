837ad8d9e0a2289ef4bdf1916845f3920373baa1
Alex Neill <alex.neill@gmail.com>
2010-12-19 13:15:19 +0000

    Adding friends and followers only for Twitter

--- lib/contacts/provider/twitter.rb
+++ lib/contacts/provider/twitter.rb
@@ -3,11 +3,31 @@ class Contacts::Provider::Twitter < Contacts::Provider::OAuth
   set_site_url 'http://api.twitter.com'
   
   def retrieve_contacts!
-    @contacts_response = connection.get("/1/statuses/friends.json")
+    {:followers_response => followers_response, :following_response => following_response}
   end
 
   def parse_contacts!
-    @contacts = MultiJson.decode(contacts_response.body).map { |contact| Contacts::Contact::Twitter.parse(contact) }.compact
+    followers & following
+  end
+  
+  def followers_response
+    @followers_response ||= connection.get("/1/statuses/followers.json")
+  end
+
+  def following_response
+    @following_response ||= connection.get("/1/statuses/friends.json")
+  end
+
+  def followers
+    @followers ||= parse_response!(followers_response)
+  end
+  
+  def following
+    @following ||= parse_response!(following_response)
+  end
+  
+  def parse_response! response
+    MultiJson.decode(response.body).map { |contact| Contacts::Contact::Twitter.parse(contact) }.compact
   end
   
 end
--- spec/models/providers/twitter_spec.rb
+++ spec/models/providers/twitter_spec.rb
@@ -25,14 +25,18 @@ describe Contacts::Provider::Twitter do
   context "when initialized" do
     before do 
       instance valid_attributes
-      @response = mock('Response')
-      @response_body = File.read(File.expand_path(File.dirname(__FILE__) + '/../../files/providers/twitter/valid_contacts')).to_s
-      @response.stub(:body).and_return(@response_body)
-      instance.connection.stub!(:get).with("/1/statuses/friends.json").and_return(@response)
+      @friends_response = mock('Response')
+      @friends_response_body = File.read(File.expand_path(File.dirname(__FILE__) + '/../../files/providers/twitter/valid_friends')).to_s
+      @friends_response.stub(:body).and_return(@friends_response_body)
+      instance.connection.stub!(:get).with("/1/statuses/friends.json").and_return(@friends_response)
+      @followers_response = mock('Response')
+      @followers_response_body = File.read(File.expand_path(File.dirname(__FILE__) + '/../../files/providers/twitter/valid_followers')).to_s
+      @followers_response.stub(:body).and_return(@followers_response_body)
+      instance.connection.stub!(:get).with("/1/statuses/followers.json").and_return(@followers_response)
     end
     
-    it "should have two contacts" do
-      instance.should have(2).contacts
+    it "should have two contact" do
+      instance.should have(1).contact
       first = instance.contacts.first
       first.should be_kind_of(Contacts::Contact::Twitter)
       first.uid.should == '169277120'
