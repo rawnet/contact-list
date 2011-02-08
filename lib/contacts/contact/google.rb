8a726f823d2e7bbbbc7421d31d5b61b945d39aa7
Alex Neill <alex.neill@gmail.com>
2011-02-08 09:39:23 +0000

    Fixing a bug when there's no email in the contact!

--- lib/contacts/contact/google.rb
+++ lib/contacts/contact/google.rb
@@ -2,7 +2,9 @@ class Contacts::Contact::Google < Struct.new(:email, :name)
 
   def self.parse xml
     name = xml.css("title").text
-    email = xml.xpath("gd:email").first['address']
+    email_node = xml.xpath("gd:email").first
+    return nil unless email_node.present?
+    email = email_node['address'] 
     new(email, name)
   end
   
